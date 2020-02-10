0190594fb725609286bfe650f2441aa1af6265ff
aidinrakanov <altynaikanatbekova@gmail.com>
2020-02-10 22:02:14 +0600

    Remake2

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -6,16 +6,46 @@ public class BankAccount {
     public double getAmount() {
         return amount;
     }
-    public void deposit (double sum){
+
+    public void deposit(double sum) {
         amount = amount + sum;
         System.out.println("сумма пополнения счета: " + amount);
+        System.out.println("_________________________");
     }
+
     public void withDraw(double sum) throws LimitException {
-        if(sum > amount){
+
+        if (sum > amount) {
             throw new LimitException("запрашиваемая сумма на снятие больше чем остаток денег на счете", amount);
         }
-        amount = amount - sum;
+        amount = getAmount() - sum;
+
         System.out.println("Сумма снятия со счета: " + sum);
 
+        System.out.println("Остаток на счете: " + getAmount());
+
+        System.out.println("_________________________");
+
+        if (getAmount() < sum) {
+            System.out.println("Сумма снятия со счета: " + getAmount());
+            amount = getAmount() - getAmount();
+
+
+        }
     }
+
 }
+/*try {
+                if (demirBank.getAmount() > 4000.0) {
+                    demirBank.withDraw(4000.0);
+                    System.out.println("Остаток счёта: " + demirBank.getAmount());
+                } else if(demirBank.getAmount() > 2000.0) {
+                    demirBank.withDraw(1000.0);
+                    System.out.println("Остаток счёта: " + demirBank.getAmount());
+                } else {
+                    demirBank.withDraw(1000.0);
+                }
+            } catch (LimitException e) {
+                e.printStackTrace();
+                break;
+            }*/
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,14 @@ public class Main {
         BankAccount demirBank = new BankAccount();
         demirBank.deposit(10000.0);
 
-        for (int i = 0; ; i =-4000) {
+        for (int i = 0; ; i = -2400) {
             try {
-                if (demirBank.getAmount() > 4000) {
-                    demirBank.withDraw(4000.0);
-                    System.out.println("Остаток счёта: " + demirBank.getAmount());
-                } else if(demirBank.getAmount() > 2000) {
-                    demirBank.withDraw(1000);
-                    System.out.println("Остаток счёта: " + demirBank.getAmount());
-                } else {
-                    demirBank.withDraw(1000);
-                }
+                demirBank.withDraw(2400.0);
             } catch (LimitException e) {
+
                 e.printStackTrace();
+
+                System.out.println(e.getMessage());
                 break;
             }
         }
